9916f3968df20358e599dca625e1a70f168b35a3
Estefany-SP <104262245+Estefany-SP@users.noreply.github.com>
2022-04-23 10:02:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ Introduccion:
 El índice de masa corporal (IMC) es un número que se calcula con base en el peso y la estatura de la persona. Para la mayoría de las personas, el IMC es un indicador confiable de la gordura y se usa para identificar las categorías de peso que pueden llevar a problemas de salud.''')
 st.image('https://www.lavanguardia.com/files/article_main_microformat/uploads/2017/06/16/5fa3c57bc8fa5.jpeg')
 st.latex(r'''IMC = \frac{peso}{estatura^2}''')
-peso = st.number_imput('Escriba su peso en Kg',50)
+peso = st.number_input('Escriba su peso en Kg',50)
 st.write(peso)
-estatura = st.number_imput('Escriba su estatura en metros',1.6)
+estatura = st.number_input('Escriba su estatura en metros',1.6)
 st.write(estatura)
 imc = peso/estatura**2
 imc = round(imc,3)
